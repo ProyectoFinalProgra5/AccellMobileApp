13430b39b68d827250636f86a380b43ecb3d98f9
Fernando <f88396262@gmail.com>
2021-12-10 00:58:28 -0600

    Creacion de nuevos Activities

--- app/src/main/java/com/fvaldiviesok/accellmobileapp/SignInActivity.kt
+++ app/src/main/java/com/fvaldiviesok/accellmobileapp/SignInActivity.kt
@@ -1,11 +1,11 @@
 package com.fvaldiviesok.accellmobileapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class SignInActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sign_in)
     }
 }
